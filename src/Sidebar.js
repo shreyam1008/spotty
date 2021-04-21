b92306061baeae28a6ee654408f9de9fb5eac71d
Shreyam Adhikari <shreyam1008@gmail.com>
2021-04-21 20:54:33 +0545

    sidebar done

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -18,6 +18,7 @@ const Sidebar = () => {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
+
       <SidebarOption Icon={HomeIcon} title="Home" />
       <SidebarOption Icon={SearchIcon} title="Search" />
       <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
