0338472dbeedd8666a13308a5e473689d7827865
Shreyam Adhikari <shreyam1008@gmail.com>
2021-04-21 22:34:07 +0545

    main body work

--- src/Body.js
+++ src/Body.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import "./Body.css"
+import React from "react";
+import "./Body.css";
+import Header from "./Header";
 
-const Body = () => {
-    return (
-       <div className="body">
-i am body
-       </div>
-    )
-}
+const Body = ({ spotify }) => {
+  console.log(spotify);
+  return (
+    <div className="body">
+      <Header />
+      <div className="body__info">
+        <img src="" alt="" />
+        <div className="body__infoText"></div>
+        <strong>Playlist</strong>
+        <h2>discovber weekly</h2>
+        <p>description</p>
+      </div>
+    </div>
+  );
+};
 
-export default Body
+export default Body;
