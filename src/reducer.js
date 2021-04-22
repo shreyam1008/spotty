682fb266e899a1d8f75eb057ff81a8936a982a66
Shreyam Adhikari <shreyam1008@gmail.com>
2021-04-22 15:27:40 +0545

    removed

--- src/reducer.js
+++ src/reducer.js
@@ -5,8 +5,7 @@ export const initialState = {
   playing: false,
   playlists: [],
   //remove token later
-  // token:
-  //   "BQC1_WCbx8sWyu-aMvUbdzxsJfHJwpBC__gD3XRnWqeBspOBn_QO4iAXZPuiA51BnpZ9bvBFUB-qBwHUB6EOxcpvKm0017lIIfzCAt3n74lq1vlGXxc5OkEWcAM_2nK6OE4sDyQ9gLfBKatWdIPsvVQGEVfVBk1nQ-I3cgbOwnkSF3yH",
+  token: null,
 };
 
 const reducer = (state, action) => {
